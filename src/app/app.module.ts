6ad9bf80731d42a76a2b1c7ad828168ddde743e6
GDV1 <gabrieldv.dv@hotmail.com>
2021-10-19 11:36:39 -0300

    New component Task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Angular Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { TaskComponent } from './task/task.component';
 
 
 const AngularMaterial = [
@@ -17,7 +18,8 @@ const AngularMaterial = [
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
