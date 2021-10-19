6ff2f5ddf9d8accb676de245b6e86a91554f15e1
GDV1 <gabrieldv.dv@hotmail.com>
2021-10-19 11:35:30 -0300

    Toolbar Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 const AngularMaterial = [
-  
+  MatToolbarModule,
+  MatIconModule
 ]
 
 @NgModule({
